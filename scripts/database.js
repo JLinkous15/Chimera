80f2989e71040f9ffffc16b77b83fc1ee1f2fd1b
James Linkous <james.william.linkous@gmail.com>
2022-11-01 10:13:42 -0500

    first commit

---
+++ scripts/database.js
@@ -0,0 +1,37 @@
+const database = {
+    heads: [
+      { id: 1, name: "Lion", price: 5.0, isCarnivore: true },
+      { id: 2, name: "Eagle", price: 7.5, isCarnivore: true },
+      { id: 3, name: "Snake", price: 2.5, isCarnivore: true },
+      { id: 4, name: "Goat", price: 1.49, isCarnivore: false }
+    ],
+    torsos: [
+      { id: 1, name: "Camel", price: 0.45, hasWings: false },
+      { id: 2, name: "Bear", price: 0.35, hasWings: false },
+      { id: 3, name: "Lion", price: 0.29, hasWings: false },
+      { id: 4, name: "Eagle", price: 0.62, hasWings: true }
+    ],
+    legs: [
+      { id: 1, name: "Horse", price: 3.79, isBipedal: false },
+      { id: 2, name: "Lizard", price: 2.5, isBipedal: false },
+      { id: 3, name: "Ostrich", price: 1.99, isBipedal: true }
+    ],
+    tails: [
+      { id: 1, name: "Scorpion", price: 0.8, isVenomous: true },
+      { id: 2, name: "Peacock", price: 0.72, isVenomous: false },
+      { id: 3, name: "Lion", price: 0.94, isVenomous: false },
+      { id: 4, name: "Lemur", price: 0.61, isVenomous: false }
+    ],
+    arms: [
+      { id: 1, name: "Scorpion", price: 0.05, hasFingers: false },
+      { id: 2, name: "Gorilla", price: 0.07, hasFingers: true },
+      { id: 3, name: "Sloth", price: 0.03, hasFingers: true },
+      { id: 4, name: "Praying Mantis", price: 0.09, hasFingers: false }
+    ],
+    powers: [
+      { id: 1, size: "Lightning Breath" },
+      { id: 2, size: "Ice Breath" },
+      { id: 3, size: "Fire Breath" }
+    ]
+  };
+  
